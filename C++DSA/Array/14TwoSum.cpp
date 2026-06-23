570568d0e3dfa60331e1c587c9e2d6a8f7dc8a67
mridulayadav07 <yadavmridula31@gmail.com>
2026-06-23 21:55:55 +0530

    Refactor code structure for improved readability and maintainability

--- C++DSA/Array/14TwoSum.cpp
+++ C++DSA/Array/14TwoSum.cpp
@@ -1,5 +1,68 @@
 #include<bits/stdc++.h>
  using namespace std;
+ //     class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         map<int,int> mpp;
+//         int n = nums.size();
+//         for(int i = 0; i < n;i++){
+//             int num = nums[i];
+//             int moreNeeded = target - num;
+//             if(mpp.find(moreNeeded) != mpp.end()){
+//                 return {mpp[moreNeeded],i};
+//             }
+//             mpp[num] = i;
+//         }
+//         return {-1,-1};
+//     }
+// };
+    //  string twoSumExists(vector<int> arr, int target) {
+    //     int n = arr.size();
+    //     // Create a vector of pairs to remember original indices after sorting
+    //     vector<pair<int, int>> numsWithIndex;
+    //     for (int i = 0; i < n; i++) {
+    //         numsWithIndex.push_back({arr[i], i});
+    //     }
+    //     // Sort based on the element values
+    //     sort(numsWithIndex.begin(), numsWithIndex.end());
+
+    //     int left = 0, right = n - 1;
+    //     // Loop until pointers cross
+    //     while (left < right) {
+    //         int sum = numsWithIndex[left].first + numsWithIndex[right].first;
+    //         if (sum == target) {
+    //             return "YES";  // Pair found
+    //         } else if (sum < target) {
+    //             left++;  // Need bigger sum, move left pointer right
+    //         } else {
+    //             right--; // Need smaller sum, move right pointer left
+    //         }
+    //     }
+    //     return "NO";  // No pair found
+    // }
+
+    // // Variant 2: Return original indices of two numbers that sum to target
+    // vector<int> twoSumIndices(vector<int> arr, int target) {
+    //     int n = arr.size();
+    //     vector<pair<int, int>> numsWithIndex;
+    //     for (int i = 0; i < n; i++) {
+    //         numsWithIndex.push_back({arr[i], i});
+    //     }
+    //     sort(numsWithIndex.begin(), numsWithIndex.end());
+
+    //     int left = 0, right = n - 1;
+    //     while (left < right) {
+    //         int sum = numsWithIndex[left].first + numsWithIndex[right].first;
+    //         if (sum == target) {
+    //             // Return original indices
+    //             return {numsWithIndex[left].second, numsWithIndex[right].second};
+    //         } else if (sum < target) {
+    //             left++;
+    //         } else {
+    //             right--;
+    //         }
+    //     }
+    //     return {-1, -1};  // No pair found
 vector<int> TwoSum(vector<int>& arr, int target ) {
      for (int i = 0; i< arr.size(); i++){
             for (int j = i + 1; j < arr.size(); j++){
--- C++DSA/Array/29FourSum.cpp
+++ C++DSA/Array/29FourSum.cpp
@@ -1,15 +1,75 @@
 #include<bits/stdc++.h>
  using namespace std;
-vector<int> TwoSum(vector<int>& arr, int target ) {
-     for (int i = 0; i< arr.size(); i++){
-            for (int j = i + 1; j < arr.size(); j++){
-                if (arr[j] == target - arr[i]){
-                    return {i , j};
+vector<vector<int>> FourSum(vector<int>& nums, int target ) {
+     int n = nums.size();
+    // set<vector<int>> ste;
+        // for (int i = 0; i< n; i++){
+        //     for (int j = i + 1; j < n; j++){
+        //         for(int k = j + 1; k < n; k++){
+        //             for(int l = k + 1; l < n; l++){
+        //                 long long sum = nums[i] + nums[j];
+        //                 sum += nums[k];
+        //                 sum += nums[l];
+        //                 if(sum == target){
+        //                     vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+        //                     sort(temp.begin(), temp.end());
+        //                     ste.insert(temp);
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     }
+        // vector<vector<int>> ans(ste.begin(), ste.end());
+        // return ans;
+
+        // set<vector<int>> ste;
+        //  for (int i = 0; i< n; i++){
+        //      for (int j = i + 1; j < n; j++){
+        //         set<long long> hashset;
+        //          for(int k = j + 1; k < n; k++){
+        //             long long sum = (nums[i] + nums[j] );
+        //             sum +=  nums[k];
+        //             long long fourth = target - sum;
+        //             if(hashset.find(fourth) != hashset.end()){
+        //                 vector<int> temp = {nums[i], nums[j], nums[k], (int)fourth};
+        //                 sort(temp.begin(), temp.end());
+        //                 ste.insert(temp);
+        //                 }
+        //                 hashset.insert(nums[k]);
+        //              }
+        //          }
+        //      }
+        //  vector<vector<int>> ans(ste.begin(), ste.end());
+        //  return ans;
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            for(int j = i + 1; j < n; j++){
+                if(j != (i+1) && nums[j] == nums[j - 1]) continue;
+                int k = j + 1;
+                int l = n - 1;
+                while(k < l){
+                    long long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if(sum == target){
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        ans.push_back(temp);
+                        k++;
+                        l--;
+                        while(k < l && nums[k] == nums[k - 1]) k++;
+                        while(k < l && nums[l] == nums[l + 1]) l--;
+                    }
+                    else if(sum < target) k++;
+                    else l--;
                 }
             }
         }
-        return {};
+        return ans;
     }
+
  int main(){
     int n;
     cout << "Enter the size of array:";
@@ -22,6 +82,6 @@ vector<int> TwoSum(vector<int>& arr, int target ) {
     int target;
     cout << "Enter the target sum: ";
     cin >> target;
-    TwoSum(arr,target);
+    FourSum(arr,target);
     return 0;
  } 
